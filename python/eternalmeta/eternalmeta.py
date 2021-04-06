fa571c4e8a631717afd7581d6e516f94a0a579db
gryfi <gabriel_14@kharisma.ac.id>
2021-04-07 00:07:16 +0800

    1st testing current dir & rename

--- python/eternalmeta/eternalmeta.py
+++ python/eternalmeta/eternalmeta.py
@@ -1,18 +1,19 @@
 #python 3.8.8
 #win 10 64 bit
-import os, time
+import os, time, glob
 
 if (False): #will be used on next
     print("Last modified: %s" % time.ctime(os.path.getmtime(v_namefile)))
     print("Created: %s" % time.ctime(os.path.getctime(v_namefile)))
 
-#put new string[basis] for addstrren
+#put new string[basis] for addsros
 #EXAMPLE
 #1. addstr("namefile.txt","additional text")
 #2. addstr(r"e:\folder\namefile.txt","additional text")
 #TODO
 #1. handle if symbol dot "." is not exist
 #2. handle if symbol dot "." is exist but not extension for example more space "namefile . e xt"
+#3. now only put before extension, next can be choose beginning or end name file
 def addstr(v_namefile,v_add_info,v_bool_is_ext = True):
     #to add space if add info is exist. is it necessary?
     v_add_info_raw = ""
@@ -37,3 +38,36 @@ def addsros(v_namefile,v_add_info,v_bool_is_ext = True):
     v_res = addstr(v_namefile,v_add_info,v_bool_is_ext)
     os.rename(v_namefile,v_res)
 
+#TODO func
+#1. to add meta data on current directory(subfolder not include)
+#2. to add meta data on one chosen folder(subfolder not include)
+#3. same as 1 and 2, but recursive on subfolder too
+#4. same as 1, 2, and 3, but use wildcard too
+#5. same as all num before, but can choose to rename file only or folder only or both of them
+
+#abbrev from "get list" file and/or folder
+#PARAM
+#1. v_both = [ -1 = "Folder Only", 0 = "Both Folder File", 1 = "File only" ]
+#   determine which one need to be add meta data on name file
+#EXAMPLE
+#PROGRESS
+#1. testing currect dir
+#ISSUE
+#1. .nomedia not detect as file?
+#TODO
+def getls(v_name_dir = "",v_is_recursive=False,v_both = 1,v_wildcard = r"*" ):
+    v_ls_file = glob.glob(os.getcwd()+r'\*')
+    if (v_both == 1):
+        v_ls_file = [f for f in v_ls_file if os.path.isfile(f)]
+    return v_ls_file
+
+#abbrev from "add string rename dir OS"
+def addsrdiros(v_add_info="",v_name_dir="",v_bool_is_ext = True,v_is_recursive=False,v_both = 1,v_wildcard = r"*" ):
+    v_ls_file = getls(v_name_dir,v_is_recursive,v_both,v_wildcard )
+    for itm_ls in v_ls_file:
+        addsros(itm_ls,v_add_info,v_bool_is_ext)
+
+#1st testing
+#print(getls())
+#addsrdiros("sent")
+#print(getls())
